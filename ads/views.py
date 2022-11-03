3d7077c2451a78a4afe38bbb61515376ebf07906
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-11-03 18:32:06 +0500

    Init

---
+++ ads/views.py
@@ -0,0 +1,12 @@
+from django.http import JsonResponse
+from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+
+
+@method_decorator(csrf_exempt, name="dispatch")
+class Index(View):
+    def get(self, request):
+
+        return JsonResponse({"status": "ok"}, status=200)
