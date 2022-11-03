d0fb6313192399c9f9600e49740cbf949c2113cb
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-11-03 19:00:29 +0500

    Create db models

--- ads/models.py
+++ ads/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Ad(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    price = models.PositiveIntegerField()
+    description = models.CharField(max_length=1000)
+    address = models.CharField(max_length=500)
+    is_published = models.BooleanField()
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
