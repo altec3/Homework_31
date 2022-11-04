dc76ecd2cce0273109d2ed9e9534e2579c154101
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-11-04 19:18:17 +0500

    Add __str__ method in db models

--- ads/models.py
+++ ads/models.py
@@ -10,7 +10,13 @@ class Ad(models.Model):
     address = models.CharField(max_length=500)
     is_published = models.BooleanField()
 
+    def __str__(self):
+        return self.name
+
 
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
