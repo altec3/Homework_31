89a2647deb2e36cb0072932ed23bc4c780dbbad9
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-11-05 17:56:47 +0500

    Do small additions and fixes

--- ads/models.py
+++ ads/models.py
@@ -6,7 +6,7 @@ class Ad(models.Model):
     name = models.CharField(max_length=50)
     author = models.CharField(max_length=50)
     price = models.PositiveIntegerField()
-    description = models.CharField(max_length=1000)
+    description = models.CharField(max_length=1000, help_text="Краткое описание. Максимум 1000 символов")
     address = models.CharField(max_length=500)
     is_published = models.BooleanField()
 
