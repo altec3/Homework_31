2defda2ac079790532d4e43567ca331f15372340
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-11-11 18:33:39 +0500

    Optimize create_fixtures.py

--- create_fixtures.py
+++ create_fixtures.py
@@ -2,24 +2,17 @@ from utils import convert_csv_to_json, create_fixtures
 
 
 if __name__ == '__main__':
+    apps = [
+        ('ads', 'ad'),
+        ('categories', 'category'),
+        ('locations', 'location'),
+        ('users', 'user')
+    ]
 
-    create_fixtures(
-        input_file=convert_csv_to_json('datasets/ad.csv'),
-        output_file='ads/fixtures/ad.json',
-        model='ads.ad'
-    )
-    create_fixtures(
-        input_file=convert_csv_to_json('datasets/category.csv'),
-        output_file='categories/fixtures/category.json',
-        model='categories.category'
-    )
-    create_fixtures(
-        input_file=convert_csv_to_json('datasets/location.csv'),
-        output_file='locations/fixtures/location.json',
-        model='locations.location'
-    )
-    create_fixtures(
-        input_file=convert_csv_to_json('datasets/user.csv'),
-        output_file='users/fixtures/user.json',
-        model='users.user'
-    )
+    for ap in apps:
+        directory, table = ap
+        create_fixtures(
+            input_file=convert_csv_to_json(f'datasets/{table}.csv'),
+            output_file=f'{directory}/fixtures/{table}.json',
+            model=f'{directory}.{table}'
+        )
