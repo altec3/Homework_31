a56313e31db75192ecc19c7c9721f433cac9e87d
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-12-01 10:59:17 +0500

    Add typehints to BooleanValueValidator

--- ads/validators.py
+++ ads/validators.py
@@ -5,6 +5,6 @@ class BooleanValueValidator:
     def __init__(self, boolean: bool = True):
         self.boolean = boolean
 
-    def __call__(self, value):
+    def __call__(self, value: bool):
         if value == self.boolean:
             raise serializers.ValidationError(f"Value must be '{not value}' only.")
