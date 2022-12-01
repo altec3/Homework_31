5df03ce4fdbcb7f0fc590d749fbbec5eb4cf139d
Aleksey Lesnikov <poprobuyu@mail.ru>
2022-12-01 10:23:42 +0500

    Add validator for Ad fields

--- ads/models.py
+++ ads/models.py
@@ -7,7 +7,12 @@ from users.models import User
 class Ad(models.Model):
     name = models.CharField(max_length=50)
     price = models.PositiveIntegerField()
-    description = models.CharField(max_length=1000, help_text="Краткое описание. Максимум 1000 символов")
+    description = models.CharField(
+        max_length=1000,
+        help_text="Краткое описание. Максимум 1000 символов",
+        blank=True,
+        null=True
+    )
     is_published = models.BooleanField(default=False)
     image = models.ImageField(upload_to="images/", null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
--- ads/serializers.py
+++ ads/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
 from ads.models import Ad
+from ads.validators import BooleanValueValidator
 
 
 class AdSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(
+        min_length=10
+    )
+
+    is_published = serializers.BooleanField(
+        validators=[BooleanValueValidator()]
+    )
+
     class Meta:
         model = Ad
         fields = "__all__"
